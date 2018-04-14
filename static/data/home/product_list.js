357bd948cb1ca0bc3277e5217cf4761e8d574ef6
duhailong <hailong.du@vcg.com>
2018-04-14 15:17:17 +0800

    update

--- static/data/home/product_list.js
+++ static/data/home/product_list.js
@@ -2,56 +2,56 @@ export default {
   status: 0,
   data: [{
       id: 1001,
-      theme: "../../images/product_list/001.gif",
+      theme: "../../images/product_list/1001.gif",
       url: "",
       name: "探索全世界只为进入你的世界",
       price: "￥6000"
     },
     {
       id: 1002,
-      theme: "../../images/product_list/002.gif",
+      theme: "../../images/product_list/1002.gif",
       url: "",
       name: "肯德基大叔",
       price: "￥3400"
     },
     {
       id: 1003,
-      theme: "../../images/product_list/001.gif",
+      theme: "../../images/product_list/1001.gif",
       url: "",
       name: "这是个牛逼的世界",
       price: "￥18000"
     },
     {
       id: 1004,
-      theme: "../../images/product_list/001.gif",
+      theme: "../../images/product_list/1001.gif",
       url: "",
       name: "这是个牛逼的世界",
       price: "￥18000"
     },
     {
       id: 1005,
-      theme: "../../images/product_list/001.gif",
+      theme: "../../images/product_list/1001.gif",
       url: "",
       name: "探索全世界只为进入你的世界",
       price: "￥6000"
     },
     {
       id: 1006,
-      theme: "../../images/product_list/002.gif",
+      theme: "../../images/product_list/1002.gif",
       url: "",
       name: "肯德基大叔",
       price: "￥3400"
     },
     {
       id: 1007,
-      theme: "../../images/product_list/001.gif",
+      theme: "../../images/product_list/1001.gif",
       url: "",
       name: "这是个牛逼的世界",
       price: "￥18000"
     },
     {
       id: 1008,
-      theme: "../../images/product_list/001.gif",
+      theme: "../../images/product_list/1001.gif",
       url: "",
       name: "这是个牛逼的世界",
       price: "￥18000"
